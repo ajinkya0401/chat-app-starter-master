62e42b034a1e7ca75284e7717566085c48399b73
AjTrash <ajinkyamane0499@gmail.com>
2023-10-10 16:28:24 +0530

    CHANGES

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { Switch, Route, BrowserRouter } from 'react-router-dom';
 
 import 'rsuite/dist/styles/rsuite-default.css';
 import './styles/main.scss';
-
+// Test
 function App() {
   return (
     <BrowserRouter>
